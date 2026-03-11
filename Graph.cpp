807a0df52bc2c96cfab9c9eadc2ed42baea9e48f
Łukasz Machnik <49206646+GoodFloor@users.noreply.github.com>
2026-03-11 16:58:17 +0100

    Initial commit

---
+++ Graph.cpp
@@ -0,0 +1,8 @@
+#include "Graph.h"
+
+#include <cstdio>
+
+void Graph::hello()
+{
+    printf("Hello world from the graph!\n");
+}
---
+++ railroadGraph.cpp
@@ -0,0 +1,13 @@
+
+#include <cstdio>
+
+#include "Graph.h"
+
+int main(int argc, char* argv[])
+{
+    printf("Hello world!\n");
+    Graph g;
+    g.hello();
+    
+    return 0;
+}
